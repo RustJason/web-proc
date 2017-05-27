30cef95f4c7a536a92e1a7118f20656eb447ffb8
Jason Young <jason@rustjason.com>
2017-05-27 16:46:16 +0800

    use hashmap

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use iron::status;
 use iron::prelude::*;
 use iron::mime::Mime;
 use params::Params;
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
 struct ProcPidStat {
@@ -21,10 +22,10 @@ struct ProcPidStat {
     cmd: String
 }
 
-#[derive(Serialize, Deserialize)]
-struct ProcInterrupts {
-    name: String,
-    values: Vec<i32>
+#[derive(Serialize)]
+struct ProcInterrupts<'a> {
+    cpu_count: usize,
+    item: HashMap<&'a str, Vec<i32>>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -52,11 +53,11 @@ fn read_file(path_str: String) -> Result<String, &'static str>{
     Ok(s)
 }
 
-fn parse_interrupts(content: String) -> Result<Vec<ProcInterrupts>, &'static str> {
+fn parse_interrupts<'a>(content: &'a String) -> Result<ProcInterrupts<'a>, &'static str> {
     let mut lines = content.lines();
     let cpu_count = lines.nth(0).unwrap().split("CPU").count() - 1;
     
-    let mut ret = Vec::new();
+    let mut item = HashMap::new();
     for line in  lines {
         if line.starts_with("ERR") || line.starts_with("MIS") {
             continue
@@ -66,10 +67,31 @@ fn parse_interrupts(content: String) -> Result<Vec<ProcInterrupts>, &'static str
             let n: String = line.chars().skip(4 + cpu_idx * 11).take(11).collect();
             val.push(n.trim().parse::<i32>().ok().unwrap());
         }
-        let r = ProcInterrupts {name: String::from(line.split_at(3).0), values: val};
-        ret.push(r);
+        //let r = (String::from(line.split_at(3).0), val);
+        item.insert(line.split_at(3).0, val);
     }
+    let ret = ProcInterrupts {cpu_count: cpu_count, item: item};
     Ok(ret)
+}
+
+fn process_interrupts() -> Result<iron::response::Response, iron::IronError> {
+    let path_str: String = String::from("/proc/interrupts");
+    let r = read_file(path_str);
+    if r.is_err() {
+        return Ok(Response::with((status::Ok, "file.open error!")));
+    }
+    let content = &r.unwrap();
+    let r = parse_interrupts(content);
+    if r.is_err() {
+        return Ok(Response::with((status::Ok, "parse_interrupts error!")));
+    }
+    let out = serde_json::to_string(&r.unwrap());
+    let content_type = "application/json".parse::<Mime>().unwrap();
+    if out.is_err() {
+        return Ok(Response::with((content_type, status::Ok, "tojson error")));
+    }
+    return Ok(Response::with((status::Ok, out.unwrap())));
+
 }
 
 
@@ -82,22 +104,7 @@ fn main() {
         println!("URL path: {:?}", req.url.path());
 
         if req.url.path()[0] == "interrupts" {
-            
-            path_str.push_str(req.url.path()[0]);
-            let r = read_file(path_str);
-            if r.is_err() {
-                return Ok(Response::with((status::Ok, "file.open error!")));
-            }
-            let r = parse_interrupts(r.unwrap());
-            if r.is_err() {
-                return Ok(Response::with((status::Ok, "parse_interrupts error!")));
-            }
-            let out = serde_json::to_string(&r.unwrap());
-            let content_type = "application/json".parse::<Mime>().unwrap();
-            if out.is_err() {
-                return Ok(Response::with((content_type, status::Ok, "tojson error")));
-            }
-            return Ok(Response::with((status::Ok, out.unwrap())));
+           return process_interrupts() 
         }
         if req.url.path()[0].parse::<i32>().is_err() {
             println!("parse int error {}", req.url.path()[0]);
